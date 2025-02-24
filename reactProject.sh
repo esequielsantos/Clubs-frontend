2985705e570d9d0f4aa66fdaf1bb0edb577b6d80
Edison Santos <edisones@ciasc.sc.gov.br>
2025-02-24 13:10:43 -0300

    Initial commit from create-react-router

---
+++ reactProject.sh
@@ -0,0 +1,20 @@
+# Crie um novo projeto React usando Vite
+npm create vite@latest {project_name} --template react
+
+# Navegue até o diretório do projeto
+cd {project_name}
+
+# Instale as dependências necessárias
+npm install @tanstack/react-query primereact primeflex primeicons typeorm @nestjs/core @nestjs/common eslint prettier
+
+# Instale as dependências de desenvolvimento para ESLint e Prettier
+npm install --save-dev eslint prettier eslint-plugin-react eslint-config-prettier eslint-plugin-prettier
+
+# Inicialize o ESLint
+npx eslint --init
+
+# Crie um arquivo de configuração do Prettier
+echo {} > .prettierrc
+
+# Adicione scripts ao package.json para ESLint e Prettier
+jq '.scripts += {"lint": "eslint .", "format": "prettier --write ."}' package.json > tmp.$$.json && mv tmp.$$.json package.json
